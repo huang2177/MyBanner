6fb588d6a1a9a8810494ce537f5778067b0c3382
USER-20170711JQ\Administrator <15378412400@163.com>
2017-09-27 11:50:38 +0800

    优化

--- app/src/main/java/com/example/administrator/mybanner/MainActivity.java
+++ app/src/main/java/com/example/administrator/mybanner/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.administrator.mybanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.huangbryant.hbanner.HBanner;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HBanner hBanner=new HBanner(this);
     }
 }
